101713d814812766bbf68d3f5613f99c3902afce
Brian C. Lane <bcl@redhat.com>
2020-12-17 13:29:59 -0800

    composer-cli: Add compose types command and test

---
+++ cmd/composer-cli/compose/types.go
@@ -0,0 +1,48 @@
+// Copyright 2020 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/spf13/cobra"
+
+	"weldr-client/cmd/composer-cli/root"
+)
+
+var (
+	typesCmd = &cobra.Command{
+		Use:   "types",
+		Short: "List the available compose types",
+		Long:  "List the available compose types",
+		RunE:  types,
+		Args:  cobra.NoArgs,
+	}
+)
+
+func init() {
+	composeCmd.AddCommand(typesCmd)
+}
+
+func types(cmd *cobra.Command, args []string) error {
+	types, resp, err := root.Client.GetComposeTypes()
+	if root.JSONOutput {
+		return nil
+	}
+	if err != nil {
+		return root.ExecutionError(cmd, "Types Error: %s", err)
+	}
+	if resp != nil && !resp.Status {
+		return root.ExecutionError(cmd, "Types Error: %s", resp.String())
+	}
+
+	sort.Strings(types)
+	for i := range types {
+		fmt.Println(types[i])
+	}
+
+	return nil
+}
---
+++ cmd/composer-cli/compose/types_test.go
@@ -0,0 +1,74 @@
+// Copyright 2020 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"weldr-client/cmd/composer-cli/root"
+)
+
+func TestCmdComposeTypes(t *testing.T) {
+	// Test the "compose types" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		var json string
+		json = `{
+    "types": [
+        {
+            "name": "ami",
+            "enabled": true
+        },
+        {
+            "name": "fedora-iot-commit",
+            "enabled": true
+        },
+        {
+            "name": "openstack",
+            "enabled": true
+        },
+        {
+            "name": "qcow2",
+            "enabled": true
+        },
+        {
+            "name": "vhd",
+            "enabled": true
+        },
+        {
+            "name": "vmdk",
+            "enabled": true
+        }
+    ]
+}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Get the compose types
+	cmd, out, err := root.ExecuteTest("compose", "types")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, typesCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "openstack")
+	assert.Contains(t, string(stdout), "qcow2")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+}
