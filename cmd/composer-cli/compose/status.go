80a099566cf701cade481eb99134544cf09ad363
Brian C. Lane <bcl@redhat.com>
2021-01-22 14:48:11 -0800

    composer-cli: Add compose status command and test

---
+++ cmd/composer-cli/compose/status.go
@@ -0,0 +1,74 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"weldr-client/weldr"
+
+	"weldr-client/cmd/composer-cli/root"
+)
+
+var (
+	statusCmd = &cobra.Command{
+		Use:   "status",
+		Short: "List the detailed status of all composes",
+		Long:  "List the detained status of all composes",
+		RunE:  status,
+		Args:  cobra.NoArgs,
+	}
+)
+
+func init() {
+	composeCmd.AddCommand(statusCmd)
+}
+
+func status(cmd *cobra.Command, args []string) (rcErr error) {
+	composes, errors, err := root.Client.ListComposes()
+	if root.JSONOutput {
+		return nil
+	}
+	if err != nil {
+		return root.ExecutionError(cmd, "List Error: %s", err)
+	}
+	if len(errors) > 0 {
+		for _, e := range errors {
+			fmt.Println(e.String())
+		}
+		rcErr = root.ExecutionError(cmd, "")
+	}
+
+	composes = weldr.SortComposeStatusV0(composes)
+	for i := range composes {
+		c := composes[i]
+
+		// Convert the API's float64 time to Time
+		var s float64
+		if c.JobFinished > 0 {
+			s = c.JobFinished
+		} else if c.JobStarted > 0 {
+			s = c.JobStarted
+		} else if c.JobCreated > 0 {
+			s = c.JobCreated
+		}
+		sec := int64(s)
+		nsec := int64(1 / (s - float64(sec)))
+		t := time.Unix(sec, nsec)
+
+		var size string
+		if c.Size > 0 {
+			size = fmt.Sprintf("%d", c.Size)
+		}
+
+		fmt.Printf("%s %-8s %s %-15s %s %-16s %s\n", c.ID, c.Status, t.Format("Mon Jan 2 15:04:05 2006"),
+			c.Blueprint, c.Version, c.Type, size)
+	}
+
+	return rcErr
+}
---
+++ cmd/composer-cli/compose/status_test.go
@@ -0,0 +1,123 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"weldr-client/cmd/composer-cli/root"
+)
+
+func TestCmdComposeStatus(t *testing.T) {
+	// Test the "compose status" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		var json string
+		if request.URL.Path == "/api/v1/compose/queue" {
+			json = `{
+	"new": [
+		{
+			"id": "b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7",
+			"blueprint": "tmux-bcl",
+			"version": "1.1.0",
+			"compose_type": "qcow2",
+			"image_size": 0,
+			"queue_status": "WAITING",
+			"job_created": 1608165958.8658934
+		}
+    ],
+    "run": [
+        {
+            "id": "6d185e04-b56e-4705-97b6-21d6c6c85f06",
+            "blueprint": "tmux-bcl",
+            "version": "1.1.0",
+            "compose_type": "qcow2",
+            "image_size": 0,
+            "queue_status": "RUNNING",
+            "job_created": 1608165945.2225826,
+            "job_started": 1608165945.2256832
+        }
+	]
+}`
+		} else if request.URL.Path == "/api/v1/compose/finished" {
+			json = `{
+	"finished": [
+		{
+			"id": "cefd01c3-629f-493e-af72-3f12981bb77b",  
+            "blueprint": "tmux-bcl",
+            "version": "1.2.0",
+            "compose_type": "qcow2",
+            "image_size": 2147483648,
+            "queue_status": "FINISHED",
+            "job_created": 1608149057.869667,
+            "job_started": 1608149057.8754315,
+            "job_finished": 1608149299.363162
+		},
+		{
+			"id": "848b6d9f-9bc3-41e1-ae33-5907ad61af76",  
+            "blueprint": "tmux-bcl",
+            "version": "1.0.0",
+            "compose_type": "qcow2",
+            "image_size": 2147483648,
+            "queue_status": "FINISHED",
+            "job_created": 1608149057.869667,
+            "job_started": 1608149057.8754315,
+            "job_finished": 1608149299.363162
+		}
+	]
+}`
+		} else if request.URL.Path == "/api/v1/compose/failed" {
+			json = `{
+	"failed": [
+		{
+            "id": "d5903571-55e2-4a18-8643-2d90611fcb11",
+            "blueprint": "tmux-bcl",
+            "version": "1.2.0",
+            "compose_type": "qcow2",
+            "image_size": 0,
+            "queue_status": "FAILED",
+            "job_created": 1608166871.5434942,
+            "job_started": 1608166871.5473683,
+            "job_finished": 1608166975.8688467
+		}
+	]
+}`
+		}
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// List the status of all of the composes
+	cmd, out, err := root.ExecuteTest("compose", "status")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, statusCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+
+	// Yes, the string contains trailing spaces.
+	assert.Equal(t, string(stdout),
+		`6d185e04-b56e-4705-97b6-21d6c6c85f06 RUNNING  Wed Dec 16 16:45:45 2020 tmux-bcl        1.1.0 qcow2            
+b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7 WAITING  Wed Dec 16 16:45:58 2020 tmux-bcl        1.1.0 qcow2            
+848b6d9f-9bc3-41e1-ae33-5907ad61af76 FINISHED Wed Dec 16 12:08:19 2020 tmux-bcl        1.0.0 qcow2            2147483648
+cefd01c3-629f-493e-af72-3f12981bb77b FINISHED Wed Dec 16 12:08:19 2020 tmux-bcl        1.2.0 qcow2            2147483648
+d5903571-55e2-4a18-8643-2d90611fcb11 FAILED   Wed Dec 16 17:02:55 2020 tmux-bcl        1.2.0 qcow2            
+`)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+}
