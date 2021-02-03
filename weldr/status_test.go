ea6f50e10f106867819fc326ba7d841118fc4258
Brian C. Lane <bcl@redhat.com>
2021-02-02 16:29:34 -0800

    weldr: Use integration tests for status test

--- weldr/status_test.go
+++ weldr/status_test.go
@@ -1,14 +1,12 @@
-// Copyright 2020 by Red Hat, Inc. All rights reserved.
+// Copyright 2020-2021 by Red Hat, Inc. All rights reserved.
 // Use of this source is goverend by the Apache License
 // that can be found in the LICENSE file.
+
+// +build integration
 
 package weldr
 
 import (
-	"bytes"
-	"context"
-	"io/ioutil"
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,21 +14,7 @@ import (
 )
 
 func TestServerStatus(t *testing.T) {
-	// Test the ServerStatus function
-	mc := MockClient{
-		DoFunc: func(request *http.Request) (*http.Response, error) {
-			var json string
-			json = `{"api":"1","db_supported":true,"db_version":"0","schema_version":"0","backend":"osbuild-composer","build":"devel","msgs":[]}`
-
-			return &http.Response{
-				StatusCode: 200,
-				Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
-			}, nil
-		},
-	}
-	tc := NewClient(context.Background(), &mc, 1, "")
-
-	status, r, err := tc.ServerStatus()
+	status, r, err := testState.client.ServerStatus()
 	require.Nil(t, err)
 	require.Nil(t, r)
 	require.NotNil(t, status)
@@ -39,6 +23,4 @@ func TestServerStatus(t *testing.T) {
 	assert.Equal(t, "osbuild-composer", status.Backend)
 	assert.Equal(t, "devel", status.Build)
 	assert.Equal(t, []string(nil), status.Messages)
-	assert.Equal(t, "GET", mc.Req.Method)
-	assert.Equal(t, "/api/status", mc.Req.URL.Path)
 }
