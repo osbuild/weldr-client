51b7f974e0aeb6da9d3facda118f88fc2ab51c20
Brian C. Lane <bcl@redhat.com>
2021-02-10 11:30:50 -0800

    blueprints: Add tests for blueprints delete command

---
+++ cmd/composer-cli/blueprints/delete_test.go
@@ -0,0 +1,78 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package blueprints
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"weldr-client/cmd/composer-cli/root"
+)
+
+func TestCmdBlueprintsDelete(t *testing.T) {
+	// Test the "blueprints delete" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{"status": true}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	cmd, out, err := root.ExecuteTest("blueprints", "delete", "cli-test-bp-1")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, deleteCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "DELETE", mc.Req.Method)
+	assert.Equal(t, "/api/v1/blueprints/delete/cli-test-bp-1", mc.Req.URL.Path)
+}
+
+func TestCmdBlueprintsDeleteUnknown(t *testing.T) {
+	// Test the "blueprints delete" command with an unknown blueprint
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+"errors": [{
+	"id": "BlueprintsError",
+	"msg": "Unknown blueprint: foo-bp-1"
+	}],
+"status": false                                        
+}`
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	cmd, out, err := root.ExecuteTest("blueprints", "delete", "foo-bp-1")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, deleteCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "DELETE", mc.Req.Method)
+	assert.Equal(t, "/api/v1/blueprints/delete/foo-bp-1", mc.Req.URL.Path)
+}
