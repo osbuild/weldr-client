0cfd331fe912e22125cea53e261884a5f72dae89
Brian C. Lane <bcl@redhat.com>
2021-02-15 11:25:23 -0800

    tests: Remove localtime from compose status test

--- cmd/composer-cli/compose/status_test.go
+++ cmd/composer-cli/compose/status_test.go
@@ -108,14 +108,15 @@ func TestCmdComposeStatus(t *testing.T) {
 	stdout, err := ioutil.ReadAll(out.Stdout)
 	assert.Nil(t, err)
 
-	// Yes, the string contains trailing spaces.
-	assert.Equal(t, string(stdout),
-		`6d185e04-b56e-4705-97b6-21d6c6c85f06 RUNNING  Wed Dec 16 16:45:45 2020 tmux-bcl        1.1.0 qcow2            
-b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7 WAITING  Wed Dec 16 16:45:58 2020 tmux-bcl        1.1.0 qcow2            
-848b6d9f-9bc3-41e1-ae33-5907ad61af76 FINISHED Wed Dec 16 12:08:19 2020 tmux-bcl        1.0.0 qcow2            2147483648
-cefd01c3-629f-493e-af72-3f12981bb77b FINISHED Wed Dec 16 12:08:19 2020 tmux-bcl        1.2.0 qcow2            2147483648
-d5903571-55e2-4a18-8643-2d90611fcb11 FAILED   Wed Dec 16 17:02:55 2020 tmux-bcl        1.2.0 qcow2            
-`)
+	// Check for expected output, but not exact match due to local time being used.
+	for _, s := range []string{"6d185e04-b56e-4705-97b6-21d6c6c85f06 RUNNING",
+		"b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7 WAITING",
+		"848b6d9f-9bc3-41e1-ae33-5907ad61af76 FINISHED",
+		"cefd01c3-629f-493e-af72-3f12981bb77b FINISHED",
+		"d5903571-55e2-4a18-8643-2d90611fcb11 FAILED"} {
+
+		assert.Contains(t, string(stdout), s)
+	}
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
