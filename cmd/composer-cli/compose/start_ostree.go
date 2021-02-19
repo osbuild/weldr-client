7a91b9555d209aa3b0a453e8a88de9d89d1bf054
Brian C. Lane <bcl@redhat.com>
2021-02-19 10:23:09 -0800

    composer-cli: Add compose start-ostree command

---
+++ cmd/composer-cli/compose/start_ostree.go
@@ -0,0 +1,69 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/weldr/weldr-client/cmd/composer-cli/root"
+	"github.com/weldr/weldr-client/weldr"
+)
+
+var (
+	startOSTreeCmd = &cobra.Command{
+		Use:   "start-ostree BLUEPRINT TYPE [IMAGE-NAME PROFILE.TOML]",
+		Short: "Start an ostree compose using the selected blueprint and output type",
+		Long:  "Start an ostree compose using the selected blueprint and output type. Optionally start an upload. --size is supported by osbuild-composer, and is in MiB",
+		RunE:  startOSTree,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 2 || len(args) == 4 {
+				return nil
+			}
+			return errors.New("Invalid number of arguments")
+		},
+	}
+	ref    string
+	parent string
+	url    string
+)
+
+func init() {
+	// size is defined in start.go
+	startOSTreeCmd.Flags().UintVarP(&size, "size", "", 0, "Size of image in MiB")
+
+	startOSTreeCmd.Flags().StringVarP(&ref, "ref", "", "", "OSTree reference")
+	startOSTreeCmd.Flags().StringVarP(&parent, "parent", "", "", "OSTree parent")
+	startOSTreeCmd.Flags().StringVarP(&url, "url", "", "", "OSTree url")
+	composeCmd.AddCommand(startOSTreeCmd)
+}
+
+func startOSTree(cmd *cobra.Command, args []string) error {
+	var resp *weldr.APIResponse
+	var uuid string
+	var err error
+	// 2 args is uploads
+	if len(args) == 2 {
+		uuid, resp, err = root.Client.StartOSTreeCompose(args[0], args[1], ref, parent, url, size)
+	} else if len(args) == 4 {
+		uuid, resp, err = root.Client.StartOSTreeComposeUpload(args[0], args[1], args[2], args[3], ref, parent, url, size)
+
+	}
+	if err != nil {
+		return root.ExecutionError(cmd, "Problem starting OSTree compose: %s", err)
+	}
+	if root.JSONOutput {
+		return nil
+	}
+	if resp != nil && !resp.Status {
+		return root.ExecutionError(cmd, strings.Join(resp.AllErrors(), "\n"))
+	}
+
+	fmt.Printf("Compose %s added to the queue\n", uuid)
+	return nil
+}
---
+++ cmd/composer-cli/compose/start_ostree_test.go
@@ -0,0 +1,208 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/weldr/weldr-client/cmd/composer-cli/root"
+)
+
+func TestCmdComposeStartOSTree(t *testing.T) {
+	// Test the "compose start-ostree" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		var json string
+		json = `{
+			"build_id": "876b2946-16cd-4f38-bace-0cdd0093d112",
+			"status": true
+}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Make sure the optional command values are reset to their defaults
+	size = 0
+	ref = ""
+	parent = ""
+	url = ""
+
+	// Start a compose
+	cmd, out, err := root.ExecuteTest("compose", "start-ostree", "--ref", "refid", "--parent", "parentid", "http-server", "qcow2")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, startOSTreeCmd)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("Compose 876b2946-16cd-4f38-bace-0cdd0093d112 added to the queue\n"), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "POST", mc.Req.Method)
+	sentBody, err := ioutil.ReadAll(mc.Req.Body)
+	mc.Req.Body.Close()
+	require.Nil(t, err)
+	assert.Equal(t, []byte(`{"blueprint_name":"http-server","compose_type":"qcow2","branch":"master","size":0,"ostree":{"ref":"refid","parent":"parentid","url":""}}`), sentBody)
+	assert.Equal(t, "application/json", mc.Req.Header.Get("Content-Type"))
+	assert.Equal(t, "/api/v1/compose", mc.Req.URL.Path)
+}
+
+func TestCmdComposeStartOSTreeURL(t *testing.T) {
+	// Test the "compose start-ostree" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		var json string
+		json = `{
+			"build_id": "876b2946-16cd-4f38-bace-0cdd0093d112",
+			"status": true
+}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Make sure the optional command values are reset to their defaults
+	size = 0
+	ref = ""
+	parent = ""
+	url = ""
+
+	// Start a compose
+	cmd, out, err := root.ExecuteTest("compose", "start-ostree", "--ref", "refid", "--url", "http://ostree-url", "http-server", "qcow2")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, startOSTreeCmd)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("Compose 876b2946-16cd-4f38-bace-0cdd0093d112 added to the queue\n"), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "POST", mc.Req.Method)
+	sentBody, err := ioutil.ReadAll(mc.Req.Body)
+	mc.Req.Body.Close()
+	require.Nil(t, err)
+	assert.Equal(t, []byte(`{"blueprint_name":"http-server","compose_type":"qcow2","branch":"master","size":0,"ostree":{"ref":"refid","parent":"","url":"http://ostree-url"}}`), sentBody)
+	assert.Equal(t, "application/json", mc.Req.Header.Get("Content-Type"))
+	assert.Equal(t, "/api/v1/compose", mc.Req.URL.Path)
+}
+
+func TestCmdComposeStartOSTreeSize(t *testing.T) {
+	// Test the "compose start" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		var json string
+		json = `{
+			"build_id": "876b2946-16cd-4f38-bace-0cdd0093d112",
+			"status": true
+}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Make sure the optional command values are reset to their defaults
+	size = 0
+	ref = ""
+	parent = ""
+	url = ""
+
+	// Start a compose
+	cmd, out, err := root.ExecuteTest("compose", "start-ostree", "--size", "998", "--ref", "refid", "--parent", "parentid", "http-server", "qcow2")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, startOSTreeCmd)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("Compose 876b2946-16cd-4f38-bace-0cdd0093d112 added to the queue\n"), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "POST", mc.Req.Method)
+	sentBody, err := ioutil.ReadAll(mc.Req.Body)
+	mc.Req.Body.Close()
+	require.Nil(t, err)
+	assert.Equal(t, []byte(`{"blueprint_name":"http-server","compose_type":"qcow2","branch":"master","size":998,"ostree":{"ref":"refid","parent":"parentid","url":""}}`), sentBody)
+	assert.Equal(t, "application/json", mc.Req.Header.Get("Content-Type"))
+	assert.Equal(t, "/api/v1/compose", mc.Req.URL.Path)
+}
+
+func TestCmdComposeStartOSTreeUpload(t *testing.T) {
+	// Test the "compose start" command with upload
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		var json string
+		json = `{
+			"build_id": "876b2946-16cd-4f38-bace-0cdd0093d112",
+			"status": true
+}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Need a temporary test file
+	tmpProfile, err := ioutil.TempFile("", "test-profile-p*.toml")
+	require.Nil(t, err)
+	defer os.Remove(tmpProfile.Name())
+
+	_, err = tmpProfile.Write([]byte(`provider = "aws"
+[settings]
+aws_access_key = "AWS Access Key"
+aws_bucket = "AWS Bucket"
+aws_region = "AWS Region"
+aws_secret_key = "AWS Secret Key"
+`))
+	require.Nil(t, err)
+
+	// Make sure the optional command values are reset to their defaults
+	size = 0
+	ref = ""
+	parent = ""
+	url = ""
+
+	// Start a compose
+	cmd, out, err := root.ExecuteTest("compose", "start-ostree", "--ref", "refid", "--parent", "parentid", "http-server", "qcow2", "httpimage", tmpProfile.Name())
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, startOSTreeCmd)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("Compose 876b2946-16cd-4f38-bace-0cdd0093d112 added to the queue\n"), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "POST", mc.Req.Method)
+	sentBody, err := ioutil.ReadAll(mc.Req.Body)
+	mc.Req.Body.Close()
+	require.Nil(t, err)
+	assert.Equal(t, []byte(`{"blueprint_name":"http-server","compose_type":"qcow2","branch":"master","size":0,"ostree":{"ref":"refid","parent":"parentid","url":""},"upload":{"provider":"aws","image_name":"httpimage","settings":{"aws_access_key":"AWS Access Key","aws_bucket":"AWS Bucket","aws_region":"AWS Region","aws_secret_key":"AWS Secret Key"}}}`), sentBody)
+	assert.Equal(t, "application/json", mc.Req.Header.Get("Content-Type"))
+	assert.Equal(t, "/api/v1/compose", mc.Req.URL.Path)
+}
