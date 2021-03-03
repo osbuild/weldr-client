9dca34ab99a06e646c07ddecca3b50814b501160
Brian C. Lane <bcl@redhat.com>
2021-03-02 16:35:13 -0800

    composer-cli: Add compose metadata command and test

---
+++ cmd/composer-cli/compose/metadata.go
@@ -0,0 +1,53 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/weldr/weldr-client/cmd/composer-cli/root"
+	"github.com/weldr/weldr-client/weldr"
+)
+
+var (
+	metadataCmd = &cobra.Command{
+		Use:   "metadata UUID",
+		Short: "Get a tar of the the metadata for the compose",
+		Long:  "Get a tar of the the metadata for the compose",
+		RunE:  getMetadata,
+		Args:  cobra.ExactArgs(1),
+	}
+)
+
+func init() {
+	composeCmd.AddCommand(metadataCmd)
+}
+
+func getMetadata(cmd *cobra.Command, args []string) (rcErr error) {
+	tf, fn, _, resp, err := root.Client.ComposeMetadata(args[0])
+	if err != nil {
+		return root.ExecutionError(cmd, "Metadata error: %s", err)
+	}
+	if resp != nil && !resp.Status {
+		return root.ExecutionError(cmd, "Metadata error: %s", resp.String())
+	}
+
+	// Move the temporary file to the server provided filename in the current directory
+	// if it doesn't already exist.
+	_, err = os.Stat(fn)
+	if err == nil {
+		os.Remove(tf)
+		return root.ExecutionError(cmd, "%s already exists", fn)
+	}
+
+	err = weldr.MoveFile(tf, fn)
+	if err != nil {
+		return root.ExecutionError(cmd, "problem moving file: %s", err)
+	}
+
+	return nil
+}
---
+++ cmd/composer-cli/compose/metadata_test.go
@@ -0,0 +1,70 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/weldr/weldr-client/cmd/composer-cli/root"
+)
+
+func TestCmdComposeMetadata(t *testing.T) {
+	// Test the "compose metadata" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		log := `This is a poor approximation of a logfile.
+But it has multiple lines.
+And should do the job.`
+
+		tar, err := root.MakeTarBytes("b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7.json", log)
+		require.Nil(t, err)
+
+		resp := http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader(tar)),
+			Header:     http.Header{},
+		}
+		resp.Header.Set("Content-Disposition", "attachment; filename=b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7-metadata.tar")
+		resp.Header.Set("Content-Type", "application/x-tar")
+
+		return &resp, nil
+	})
+
+	// Change to a temporary directory for the file to be saved in
+	dir, err := ioutil.TempDir("", "test-metadata-*")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	prevDir, _ := os.Getwd()
+	err = os.Chdir(dir)
+	require.Nil(t, err)
+	defer os.Chdir(prevDir)
+
+	// Get the logs
+	cmd, out, err := root.ExecuteTest("compose", "metadata", "b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, metadataCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/compose/metadata/b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7", mc.Req.URL.Path)
+
+	_, err = os.Stat("b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7-metadata.tar")
+	assert.Nil(t, err)
+}
