1cd65f035523ed11a2880ba1a30d3d4b3c398a0e
Brian C. Lane <bcl@redhat.com>
2021-03-02 17:40:29 -0800

    composer-cli: Add compose image command and tests

---
+++ cmd/composer-cli/compose/image.go
@@ -0,0 +1,53 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/weldr/weldr-client/cmd/composer-cli/root"
+	"github.com/weldr/weldr-client/weldr"
+)
+
+var (
+	imageCmd = &cobra.Command{
+		Use:   "image UUID",
+		Short: "Get the compose image file",
+		Long:  "Get the compose image file",
+		RunE:  getImage,
+		Args:  cobra.ExactArgs(1),
+	}
+)
+
+func init() {
+	composeCmd.AddCommand(imageCmd)
+}
+
+func getImage(cmd *cobra.Command, args []string) (rcErr error) {
+	tf, fn, _, resp, err := root.Client.ComposeImage(args[0])
+	if err != nil {
+		return root.ExecutionError(cmd, "Image error: %s", err)
+	}
+	if resp != nil && !resp.Status {
+		return root.ExecutionError(cmd, "Image error: %s", resp.String())
+	}
+
+	// Move the temporary file to the server provided filename in the current directory
+	// if it doesn't already exist.
+	_, err = os.Stat(fn)
+	if err == nil {
+		os.Remove(tf)
+		return root.ExecutionError(cmd, "%s already exists", fn)
+	}
+
+	err = weldr.MoveFile(tf, fn)
+	if err != nil {
+		return root.ExecutionError(cmd, "problem moving file: %s", err)
+	}
+
+	return nil
+}
---
+++ cmd/composer-cli/compose/image_test.go
@@ -0,0 +1,67 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+package compose
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/weldr/weldr-client/cmd/composer-cli/root"
+)
+
+func TestCmdComposeImage(t *testing.T) {
+	// Test the "compose image" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		data := `This is a poor approximation of an image file.`
+
+		resp := http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(data))),
+			Header:     http.Header{},
+		}
+		resp.Header.Set("Content-Disposition", "attachment; filename=b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7.qcow2")
+		resp.Header.Set("Content-Type", "application/octet-stream")
+		resp.Header.Set("Content-Length", fmt.Sprintf("%d", len(data)))
+
+		return &resp, nil
+	})
+
+	// Change to a temporary directory for the file to be saved in
+	dir, err := ioutil.TempDir("", "test-image-*")
+	require.Nil(t, err)
+	defer os.RemoveAll(dir)
+
+	prevDir, _ := os.Getwd()
+	err = os.Chdir(dir)
+	require.Nil(t, err)
+	defer os.Chdir(prevDir)
+
+	// Get the logs
+	cmd, out, err := root.ExecuteTest("compose", "image", "b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, imageCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/compose/image/b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7", mc.Req.URL.Path)
+
+	_, err = os.Stat("b27c5a7b-d1f6-4c8c-8526-6d6de464f1c7.qcow2")
+	assert.Nil(t, err)
+}
