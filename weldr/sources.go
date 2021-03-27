62a7cc4af5f7df250a926c7d9afb7221b4dc23c4
Brian C. Lane <bcl@redhat.com>
2021-03-26 18:11:00 -0700

    weldr: Add sources functions and tests
    Add functions for listing, adding, getting, and deleting project source
    repositories.

--- weldr/sources.go
+++ weldr/sources.go
@@ -1,1 +1,85 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
 package weldr
+
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// ListSources returns a list of all of the sources available
+func (c Client) ListSources() ([]string, *APIResponse, error) {
+	j, resp, err := c.GetRaw("GET", "/projects/source/list")
+	if err != nil {
+		return nil, nil, err
+	}
+	if resp != nil {
+		return nil, resp, nil
+	}
+
+	var sources struct {
+		Sources []string
+	}
+	err = json.Unmarshal(j, &sources)
+	if err != nil {
+		return nil, nil, err
+	}
+	sort.Strings(sources.Sources)
+	return sources.Sources, nil, nil
+}
+
+// GetSourcesJSON returns the sources and errors
+// It uses interface{} for the sources so that it is not tightly coupled to the server's source
+// schema.
+func (c Client) GetSourcesJSON(names []string) (map[string]interface{}, []APIErrorMsg, error) {
+	var errors []APIErrorMsg
+	route := fmt.Sprintf("/projects/source/info/%s", strings.Join(names, ","))
+	j, resp, err := c.GetRaw("GET", route)
+	if err != nil {
+		return nil, nil, err
+	}
+	if resp != nil {
+		errors = append(errors, resp.Errors...)
+		return nil, errors, nil
+	}
+
+	// flexible source unmarshaling, be strict about the error message
+	var r struct {
+		Sources map[string]interface{} `json:"sources"`
+		Errors  []APIErrorMsg          `json:"errors"`
+	}
+	err = json.Unmarshal(j, &r)
+	if err != nil {
+		errors = append(errors, APIErrorMsg{"JSONError", err.Error()})
+	}
+	if len(errors) > 0 {
+		return nil, errors, nil
+	}
+	if len(r.Errors) > 0 {
+		errors = append(errors, r.Errors...)
+	}
+	return r.Sources, errors, nil
+}
+
+// NewSourceTOML adds (or updates if it already exists) a source using TOML
+// When successful the response will have Status = true
+func (c Client) NewSourceTOML(source string) (*APIResponse, error) {
+	body, resp, err := c.PostTOML("/projects/source/new", source)
+	// body may contain a response with status and errors
+	if resp == nil && len(body) > 0 {
+		resp, _ = NewAPIResponse(body)
+	}
+	return resp, err
+}
+
+// DeleteSource deletes a source and returns the server result
+// Note that trying to delete a system source will fail with an error
+func (c Client) DeleteSource(id string) (*APIResponse, error) {
+	route := fmt.Sprintf("/projects/source/delete/%s", id)
+	_, resp, err := c.DeleteRaw(route)
+	return resp, err
+}
--- weldr/sources_test.go
+++ weldr/sources_test.go
@@ -1,1 +1,126 @@
+// Copyright 2021 by Red Hat, Inc. All rights reserved.
+// Use of this source is goverend by the Apache License
+// that can be found in the LICENSE file.
+
+// +build integration
+
 package weldr
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestListSources(t *testing.T) {
+	sources, r, err := testState.client.ListSources()
+	require.Nil(t, err)
+	require.Nil(t, r)
+	assert.GreaterOrEqual(t, len(sources), 1)
+}
+
+func TestGetSourcesJSON(t *testing.T) {
+	// Need to use a real source name in this test, get it first
+	names, r, err := testState.client.ListSources()
+	require.Nil(t, err)
+	require.Nil(t, r)
+	require.GreaterOrEqual(t, len(names), 1)
+
+	sources, errors, err := testState.client.GetSourcesJSON([]string{names[0], "unknown"})
+	require.Nil(t, err)
+	require.NotNil(t, errors)
+	require.Equal(t, 1, len(errors))
+	assert.Equal(t, APIErrorMsg{"UnknownSource", "unknown is not a valid source"}, errors[0])
+
+	require.NotNil(t, sources)
+	require.GreaterOrEqual(t, len(sources), 1)
+	id, ok := sources[names[0]].(map[string]interface{})["id"].(string)
+	require.True(t, ok)
+	assert.Equal(t, names[0], id)
+	sourceType, ok := sources[names[0]].(map[string]interface{})["type"].(string)
+	require.True(t, ok)
+	assert.Equal(t, "yum-metalink", sourceType)
+}
+
+func TestGetSourcesJSONError(t *testing.T) {
+	sources, errors, err := testState.client.GetSourcesJSON([]string{"unknown"})
+	require.Nil(t, err)
+	require.NotNil(t, errors)
+	require.Equal(t, 1, len(errors))
+	require.Equal(t, 0, len(sources))
+	assert.Equal(t, APIErrorMsg{"UnknownSource", "unknown is not a valid source"}, errors[0])
+}
+
+func TestNewSourceTOML(t *testing.T) {
+	source := `check_gpg = true
+check_ssl = true
+id = "test-source-1"
+name = "Test new source"
+type = "yum-metalink"
+url = "https://mirrors.fedoraproject.org/metalink?repo=fedora-33&arch=x86_64"`
+	r, err := testState.client.NewSourceTOML(source)
+	require.Nil(t, err)
+	require.NotNil(t, r)
+	assert.True(t, r.Status)
+}
+
+func TestNewSourceTOMLError(t *testing.T) {
+	// Use a source that's missing a trailing '"' on name
+	source := `check_gpg = true
+check_ssl = true
+id = "test-source-1"
+name = "Test new source
+type = "yum-metalink"
+url = "https://mirrors.fedoraproject.org/metalink?repo=fedora-33&arch=x86_64"`
+	r, err := testState.client.NewSourceTOML(source)
+	require.Nil(t, err)
+	require.NotNil(t, r)
+	assert.False(t, r.Status)
+	require.Equal(t, 1, len(r.Errors))
+	assert.Equal(t, "ProjectsError", r.Errors[0].ID)
+}
+
+func TestDeleteSource(t *testing.T) {
+	// Push a source to be deleted
+	source := `check_gpg = true
+check_ssl = true
+id = "test-source-2"
+name = "Test deleting a new source"
+type = "yum-metalink"
+url = "https://mirrors.fedoraproject.org/metalink?repo=fedora-33&arch=x86_64"`
+	r, err := testState.client.NewSourceTOML(source)
+	require.Nil(t, err)
+	require.NotNil(t, r)
+	require.True(t, r.Status)
+
+	r, err = testState.client.DeleteSource("test-source-2")
+	require.Nil(t, err)
+	require.Nil(t, r)
+}
+
+// TODO: osbuild-composer returns true when deleting an unknown source
+// Change this when that is fixed
+//func TestDeleteUnknownSource(t *testing.T) {
+//	r, err := testState.client.DeleteSource("unknown")
+//	require.Nil(t, err)
+//	require.NotNil(t, r)
+//	assert.False(t, r.Status)
+//	require.Equal(t, 1, len(r.Errors))
+//	assert.Equal(t, APIErrorMsg{"ProjectsError", "Unknown blueprint: unknown-blueprint-test"}, r.Errors[0])
+//}
+
+func TestDeleteSystemSource(t *testing.T) {
+	// Need to use a real source name in this test, get it first
+	names, r, err := testState.client.ListSources()
+	require.Nil(t, err)
+	require.Nil(t, r)
+	require.GreaterOrEqual(t, len(names), 1)
+
+	r, err = testState.client.DeleteSource(names[0])
+	require.Nil(t, err)
+	require.NotNil(t, r)
+	assert.False(t, r.Status)
+	require.Equal(t, 1, len(r.Errors))
+	assert.Equal(t, "SystemSource", r.Errors[0].ID)
+}
