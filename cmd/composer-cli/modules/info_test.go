1843f72186ea7deec69acb381463ad0c18152435
Brian C. Lane <bcl@redhat.com>
2021-09-16 16:51:20 -0700

    tests: Add JSON and error result testing to modules
    This tests the changes in behavior for the --json output mode for the
    modules commands.

--- cmd/composer-cli/modules/info_test.go
+++ cmd/composer-cli/modules/info_test.go
@@ -76,7 +76,9 @@ func TestCmdModulesInfo(t *testing.T) {
 		}, nil
 	})
 
+	distro = ""
 	cmd, out, err := root.ExecuteTest("modules", "info", "bash")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.Nil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -94,6 +96,170 @@ func TestCmdModulesInfo(t *testing.T) {
 	assert.Equal(t, []byte(""), stderr)
 	assert.Equal(t, "GET", mc.Req.Method)
 	assert.Equal(t, "/api/v1/modules/info/bash", mc.Req.URL.Path)
+}
+
+func TestCmdModulesInfoJSON(t *testing.T) {
+	// Test the "modules info" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+    "modules": [
+        {
+            "builds": [
+                {
+                    "arch": "x86_64",
+                    "build_config_ref": "BUILD_CONFIG_REF",
+                    "build_env_ref": "BUILD_ENV_REF",
+                    "build_time": "2020-07-27T13:17:35",
+                    "changelog": "CHANGELOG_NEEDED",
+                    "epoch": 0,
+                    "metadata": {},
+                    "release": "2.fc33",
+                    "source": {
+                        "license": "GPLv3+",
+                        "metadata": {},
+                        "source_ref": "SOURCE_REF",
+                        "version": "5.0.17"
+                    }
+                }
+            ],
+            "dependencies": [
+                {
+                    "arch": "noarch",
+                    "check_gpg": true,
+                    "checksum": "sha256:f4efaa5bc8382246d8230ece8bacebd3c29eb9fd52b509b1e6575e643953851b",
+                    "epoch": 0,
+                    "name": "basesystem",
+                    "release": "10.fc33",
+                    "remote_location": "http://mirror.web-ster.com/fedora/releases/33/Everything/x86_64/os/Packages/b/basesystem-11-10.fc33.noarch.rpm",
+                    "version": "11"
+                },
+                {
+                    "arch": "x86_64",
+                    "check_gpg": true,
+                    "checksum": "sha256:c59a621f3cdd5e073b3c1ef9cd8fd9d7e02d77d94be05330390eac05f77b5b60",
+                    "epoch": 0,
+                    "name": "bash",
+                    "release": "2.fc33",
+                    "remote_location": "http://mirror.web-ster.com/fedora/releases/33/Everything/x86_64/os/Packages/b/bash-5.0.17-2.fc33.x86_64.rpm",
+                    "version": "5.0.17"
+                }
+            ],
+            "description": "The GNU Bourne Again shell (Bash) is a shell or command language\ninterpreter that is compatible with the Bourne shell (sh). Bash\nincorporates useful features from the Korn shell (ksh) and the C shell\n(csh). Most sh scripts can be run by bash without modification.",
+            "homepage": "https://www.gnu.org/software/bash",
+            "name": "bash",
+            "summary": "The GNU Bourne Again shell",
+            "upstream_vcs": "UPSTREAM_VCS"
+        }
+    ]}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "modules", "info", "bash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"description\": \"The GNU Bourne Again shell")
+	assert.Contains(t, string(stdout), "\"version\": \"5.0.17\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/modules/info/bash\"")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/modules/info/bash", mc.Req.URL.Path)
+}
+
+func TestCmdModulesInfoUnknown(t *testing.T) {
+	// Test the "modules info" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+        "errors": [
+            {
+                "id": "UnknownModule",
+                "msg": "No packages have been found."
+            }
+        ],
+        "status": false
+}`
+
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("modules", "info", "mash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	assert.Equal(t, root.ExecutionError(cmd, ""), err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stderr), "UnknownModule: No packages have been found")
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/modules/info/mash", mc.Req.URL.Path)
+}
+
+func TestCmdModulesInfoUnknownJSON(t *testing.T) {
+	// Test the "modules info" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+        "errors": [
+            {
+                "id": "UnknownModule",
+                "msg": "No packages have been found."
+            }
+        ],
+        "status": false
+}`
+
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "modules", "info", "mash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	assert.Equal(t, root.ExecutionError(cmd, ""), err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": false")
+	assert.Contains(t, string(stdout), "\"id\": \"UnknownModule\"")
+	assert.Contains(t, string(stdout), "\"msg\": \"No packages have been found.\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/api/v1/modules/info/mash\"")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/modules/info/mash", mc.Req.URL.Path)
 }
 
 func TestCmdModulesInfoDistro(t *testing.T) {
@@ -156,7 +322,9 @@ func TestCmdModulesInfoDistro(t *testing.T) {
 		}, nil
 	})
 
+	distro = ""
 	cmd, out, err := root.ExecuteTest("modules", "info", "--distro=test-distro", "bash")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.Nil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -169,6 +337,87 @@ func TestCmdModulesInfoDistro(t *testing.T) {
 	assert.Contains(t, string(stdout), "             shell (sh). Bash")
 	assert.Contains(t, string(stdout), "     5.0.17-2.fc33.x86_64 at")
 	assert.Contains(t, string(stdout), "     basesystem-11-10.fc33.noarch")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/modules/info/bash", mc.Req.URL.Path)
+}
+
+func TestCmdModulesInfoDistroJSON(t *testing.T) {
+	// Test the "modules info --distro=test-distro" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+    "modules": [
+        {
+            "builds": [
+                {
+                    "arch": "x86_64",
+                    "build_config_ref": "BUILD_CONFIG_REF",
+                    "build_env_ref": "BUILD_ENV_REF",
+                    "build_time": "2020-07-27T13:17:35",
+                    "changelog": "CHANGELOG_NEEDED",
+                    "epoch": 0,
+                    "metadata": {},
+                    "release": "2.fc33",
+                    "source": {
+                        "license": "GPLv3+",
+                        "metadata": {},
+                        "source_ref": "SOURCE_REF",
+                        "version": "5.0.17"
+                    }
+                }
+            ],
+            "dependencies": [
+                {
+                    "arch": "noarch",
+                    "check_gpg": true,
+                    "checksum": "sha256:f4efaa5bc8382246d8230ece8bacebd3c29eb9fd52b509b1e6575e643953851b",
+                    "epoch": 0,
+                    "name": "basesystem",
+                    "release": "10.fc33",
+                    "remote_location": "http://mirror.web-ster.com/fedora/releases/33/Everything/x86_64/os/Packages/b/basesystem-11-10.fc33.noarch.rpm",
+                    "version": "11"
+                },
+                {
+                    "arch": "x86_64",
+                    "check_gpg": true,
+                    "checksum": "sha256:c59a621f3cdd5e073b3c1ef9cd8fd9d7e02d77d94be05330390eac05f77b5b60",
+                    "epoch": 0,
+                    "name": "bash",
+                    "release": "2.fc33",
+                    "remote_location": "http://mirror.web-ster.com/fedora/releases/33/Everything/x86_64/os/Packages/b/bash-5.0.17-2.fc33.x86_64.rpm",
+                    "version": "5.0.17"
+                }
+            ],
+            "description": "The GNU Bourne Again shell (Bash) is a shell or command language\ninterpreter that is compatible with the Bourne shell (sh). Bash\nincorporates useful features from the Korn shell (ksh) and the C shell\n(csh). Most sh scripts can be run by bash without modification.",
+            "homepage": "https://www.gnu.org/software/bash",
+            "name": "bash",
+            "summary": "The GNU Bourne Again shell",
+            "upstream_vcs": "UPSTREAM_VCS"
+        }
+    ]}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "modules", "info", "--distro=test-distro", "bash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"description\": \"The GNU Bourne Again shell")
+	assert.Contains(t, string(stdout), "\"version\": \"5.0.17\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/modules/info/bash?distro=test-distro\"")
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
@@ -197,7 +446,9 @@ func TestCmdModulesInfoBadDistro(t *testing.T) {
 	})
 
 	// Get the compose types
+	distro = ""
 	cmd, out, err := root.ExecuteTest("modules", "info", "--distro=homer", "bash")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.NotNil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -210,5 +461,47 @@ func TestCmdModulesInfoBadDistro(t *testing.T) {
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Contains(t, string(stderr), "DistroError: Invalid distro: homer")
+	assert.Equal(t, "GET", mc.Req.Method)
+}
+
+func TestCmdModulesInfoBadDistroJSON(t *testing.T) {
+	// Test the "modules info --distro=homer" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+        "errors": [
+            {
+                "id": "DistroError",
+                "msg": "Invalid distro: homer"
+            }
+        ],
+        "status": false
+}`
+
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Get the compose types
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "modules", "info", "--distro=homer", "bash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": false")
+	assert.Contains(t, string(stdout), "\"id\": \"DistroError\"")
+	assert.Contains(t, string(stdout), "\"msg\": \"Invalid distro: homer\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/api/v1/modules/info/bash?distro=homer\"")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
 	assert.Equal(t, "GET", mc.Req.Method)
 }
