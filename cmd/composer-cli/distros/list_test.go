f44c61f51f5e5b4c15fa1db4293e216fd0caacc1
Brian C. Lane <bcl@redhat.com>
2021-09-16 15:44:00 -0700

    tests: Add JSON and error result testing to distros
    This tests the changes in behavior for the --json output mode for the
    distros list command.

--- cmd/composer-cli/distros/list_test.go
+++ cmd/composer-cli/distros/list_test.go
@@ -29,6 +29,7 @@ func TestCmdDistrosList(t *testing.T) {
 
 	// Get the list of distros
 	cmd, out, err := root.ExecuteTest("distros", "list")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.Nil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -37,7 +38,40 @@ func TestCmdDistrosList(t *testing.T) {
 	assert.Equal(t, cmd, listCmd)
 	stdout, err := ioutil.ReadAll(out.Stdout)
 	assert.Nil(t, err)
-	assert.NotEqual(t, []byte(""), stdout)
+	assert.NotContains(t, string(stdout), "{")
+	assert.Contains(t, string(stdout), "centos-8")
+	assert.Contains(t, string(stdout), "fedora-33")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+}
+
+func TestCmdDistrosListJSON(t *testing.T) {
+	// Test the "distros list" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{"distros":["centos-8","fedora-32","fedora-33","rhel-8"]}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Get the list of distros
+	cmd, out, err := root.ExecuteTest("--json", "distros", "list")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, listCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "{")
+	assert.Contains(t, string(stdout), "\"centos-8\"")
+	assert.Contains(t, string(stdout), "\"fedora-33\"")
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
