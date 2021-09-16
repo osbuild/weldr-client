b757fc8c68f3fc22fd1bf2c0fb548529c5c1fbf3
Brian C. Lane <bcl@redhat.com>
2021-09-16 16:51:20 -0700

    tests: Add JSON and error result testing to modules
    This tests the changes in behavior for the --json output mode for the
    modules commands.

--- cmd/composer-cli/modules/list_test.go
+++ cmd/composer-cli/modules/list_test.go
@@ -39,7 +39,9 @@ func TestCmdModulesList(t *testing.T) {
 		}, nil
 	})
 
+	distro = ""
 	cmd, out, err := root.ExecuteTest("modules", "list")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.Nil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -50,6 +52,49 @@ func TestCmdModulesList(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Contains(t, string(stdout), "http-server-prod")
 	assert.Contains(t, string(stdout), "nfs-server-test")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/modules/list", mc.Req.URL.Path)
+}
+
+func TestCmdModulesListJSON(t *testing.T) {
+	// Test the "modules list" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		query := request.URL.Query()
+		v := query.Get("limit")
+		limit, _ := strconv.ParseUint(v, 10, 64)
+		var json string
+		if limit == 0 {
+			json = `{"modules": [], "total": 2, "offset": 0, "limit": 0}`
+		} else {
+			json = `{"modules": [
+						{"name":"http-server-prod", "group_type":"rpm"},
+						{"name":"nfs-server-test", "group_type":"rpm"}],
+					"total": 2, "offset": 0, "limit": 2}`
+		}
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "modules", "list")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, listCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"name\": \"http-server-prod\"")
+	assert.Contains(t, string(stdout), "\"name\": \"nfs-server-test\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/modules/list?limit=0\"")
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
@@ -79,7 +124,9 @@ func TestCmdModulesListDistro(t *testing.T) {
 		}, nil
 	})
 
+	distro = ""
 	cmd, out, err := root.ExecuteTest("modules", "list", "--distro=test-distro")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.Nil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -90,6 +137,49 @@ func TestCmdModulesListDistro(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Contains(t, string(stdout), "http-server-prod")
 	assert.Contains(t, string(stdout), "nfs-server-test")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/modules/list", mc.Req.URL.Path)
+}
+
+func TestCmdModulesListDistroJSON(t *testing.T) {
+	// Test the "modules list --distro=test-distro" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		query := request.URL.Query()
+		v := query.Get("limit")
+		limit, _ := strconv.ParseUint(v, 10, 64)
+		var json string
+		if limit == 0 {
+			json = `{"modules": [], "total": 2, "offset": 0, "limit": 0}`
+		} else {
+			json = `{"modules": [
+						{"name":"http-server-prod", "group_type":"rpm"},
+						{"name":"nfs-server-test", "group_type":"rpm"}],
+					"total": 2, "offset": 0, "limit": 2}`
+		}
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "modules", "list", "--distro=test-distro")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, listCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"name\": \"http-server-prod\"")
+	assert.Contains(t, string(stdout), "\"name\": \"nfs-server-test\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/modules/list?distro=test-distro\\u0026limit=0\"")
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
@@ -118,7 +208,9 @@ func TestCmdModulesListBadDistro(t *testing.T) {
 	})
 
 	// Get the compose types
+	distro = ""
 	cmd, out, err := root.ExecuteTest("modules", "list", "--distro=homer")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.NotNil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -131,5 +223,47 @@ func TestCmdModulesListBadDistro(t *testing.T) {
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Contains(t, string(stderr), "DistroError: Invalid distro: homer")
+	assert.Equal(t, "GET", mc.Req.Method)
+}
+
+func TestCmdModulesListBadDistroJSON(t *testing.T) {
+	// Test the "modules list --distro=homer" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+        "errors": [
+            {
+                "id": "DistroError",
+                "msg": "Invalid distro: homer"
+            }
+        ],
+        "status": false
+}`
+
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Get the compose types
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "modules", "list", "--distro=homer")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, listCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": false")
+	assert.Contains(t, string(stdout), "\"id\": \"DistroError\"")
+	assert.Contains(t, string(stdout), "\"msg\": \"Invalid distro: homer\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/api/v1/modules/list?distro=homer\\u0026limit=0\"")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
 	assert.Equal(t, "GET", mc.Req.Method)
 }
