68ab83c0408bc4df207d1b372f509374396ece65
Brian C. Lane <bcl@redhat.com>
2021-09-17 11:37:40 -0700

    tests: Add JSON and error result testing to sources
    This tests the changes in behavior for the --json output mode for the
    sources commands.

--- cmd/composer-cli/sources/delete_test.go
+++ cmd/composer-cli/sources/delete_test.go
@@ -28,6 +28,7 @@ func TestCmdSourcesDelete(t *testing.T) {
 	})
 
 	cmd, out, err := root.ExecuteTest("sources", "delete", "test-source-1")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.Nil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -37,6 +38,35 @@ func TestCmdSourcesDelete(t *testing.T) {
 	stdout, err := ioutil.ReadAll(out.Stdout)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "DELETE", mc.Req.Method)
+	assert.Equal(t, "/api/v1/projects/source/delete/test-source-1", mc.Req.URL.Path)
+}
+
+func TestCmdSourcesDeleteJSON(t *testing.T) {
+	// Test the "sources delete" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{"status": true}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	cmd, out, err := root.ExecuteTest("--json", "sources", "delete", "test-source-1")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, deleteCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": true")
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
@@ -67,6 +97,7 @@ func TestCmdSourcesDeleteSystem(t *testing.T) {
 	})
 
 	cmd, out, err := root.ExecuteTest("sources", "delete", "fedora")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.NotNil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -79,6 +110,47 @@ func TestCmdSourcesDeleteSystem(t *testing.T) {
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Contains(t, string(stderr), "SystemSource")
+	assert.Equal(t, "DELETE", mc.Req.Method)
+	assert.Equal(t, "/api/v1/projects/source/delete/fedora", mc.Req.URL.Path)
+}
+
+func TestCmdSourcesDeleteSystemJSON(t *testing.T) {
+	// Test the "sources delete" command with a system source
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+    "errors": [
+        {
+            "id": "SystemSource",
+            "msg": "fedora is a system source, it cannot be deleted."
+        }
+    ],
+    "status": false
+}`
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	cmd, out, err := root.ExecuteTest("--json", "sources", "delete", "fedora")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, deleteCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": false")
+	assert.Contains(t, string(stdout), "\"id\": \"SystemSource\"")
+	assert.Contains(t, string(stdout), "\"msg\": \"fedora is a system source, it cannot be deleted.\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/api/v1/projects/source/delete/fedora\"")
+	assert.Contains(t, string(stdout), "\"status\": 400")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
 	assert.Equal(t, "DELETE", mc.Req.Method)
 	assert.Equal(t, "/api/v1/projects/source/delete/fedora", mc.Req.URL.Path)
 }
