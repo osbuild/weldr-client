f354205de50cfd92a9c4e864f6359ad58c9c72d0
Brian C. Lane <bcl@redhat.com>
2021-09-17 10:33:27 -0700

    tests: Add JSON and error result testing to projects
    This tests the changes in behavior for the --json output mode for the
    projects commands.

--- cmd/composer-cli/projects/depsolve_test.go
+++ cmd/composer-cli/projects/depsolve_test.go
@@ -49,6 +49,7 @@ func TestCmdProjectsDepsolve(t *testing.T) {
 	})
 
 	cmd, out, err := root.ExecuteTest("projects", "depsolve", "bash")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.Nil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -57,8 +58,60 @@ func TestCmdProjectsDepsolve(t *testing.T) {
 	assert.Equal(t, cmd, depsolveCmd)
 	stdout, err := ioutil.ReadAll(out.Stdout)
 	assert.Nil(t, err)
+	assert.NotContains(t, string(stdout), "{")
 	assert.Contains(t, string(stdout), "acl-2.2.53-9.fc33.x86_64")
 	assert.Contains(t, string(stdout), "basesystem-11-10.fc33.noarch")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+}
+
+func TestCmdProjectsDepsolveJSON(t *testing.T) {
+	// Test the "projects depsolve" command
+	json := `{
+		"projects": [
+            {
+                "arch": "x86_64",
+                "check_gpg": true,
+                "checksum": "sha256:92c1615d385b32088f78a6574a2bf89a6bb29d9858abdd71471ef5113ef0831f",
+                "epoch": 0,
+                "name": "acl",
+                "release": "9.fc33",
+                "remote_location": "https://mirrors.rit.edu/fedora/fedora/linux/releases/33/Everything/x86_64/os/Packages/a/acl-2.2.53-9.fc33.x86_64.rpm",
+                "version": "2.2.53"
+            },
+            {
+                "arch": "noarch",
+                "check_gpg": true,
+                "checksum": "sha256:f4efaa5bc8382246d8230ece8bacebd3c29eb9fd52b509b1e6575e643953851b",
+                "epoch": 0,
+                "name": "basesystem",
+                "release": "10.fc33",
+                "remote_location": "https://mirrors.rit.edu/fedora/fedora/linux/releases/33/Everything/x86_64/os/Packages/b/basesystem-11-10.fc33.noarch.rpm",
+                "version": "11"
+            }
+	]}`
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	cmd, out, err := root.ExecuteTest("--json", "projects", "depsolve", "bash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, depsolveCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"name\": \"basesystem\"")
+	assert.Contains(t, string(stdout), "\"version\": \"2.2.53\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/projects/depsolve/bash\"")
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
@@ -98,6 +151,7 @@ func TestCmdProjectsDepsolveDistro(t *testing.T) {
 	})
 
 	cmd, out, err := root.ExecuteTest("projects", "depsolve", "--distro=test-distro", "bash")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.Nil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -106,8 +160,60 @@ func TestCmdProjectsDepsolveDistro(t *testing.T) {
 	assert.Equal(t, cmd, depsolveCmd)
 	stdout, err := ioutil.ReadAll(out.Stdout)
 	assert.Nil(t, err)
+	assert.NotContains(t, string(stdout), "{")
 	assert.Contains(t, string(stdout), "acl-2.2.53-9.fc33.x86_64")
 	assert.Contains(t, string(stdout), "basesystem-11-10.fc33.noarch")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+}
+
+func TestCmdProjectsDepsolveDistroJSON(t *testing.T) {
+	// Test the "projects depsolve --distro=test-distro" command
+	json := `{
+		"projects": [
+            {
+                "arch": "x86_64",
+                "check_gpg": true,
+                "checksum": "sha256:92c1615d385b32088f78a6574a2bf89a6bb29d9858abdd71471ef5113ef0831f",
+                "epoch": 0,
+                "name": "acl",
+                "release": "9.fc33",
+                "remote_location": "https://mirrors.rit.edu/fedora/fedora/linux/releases/33/Everything/x86_64/os/Packages/a/acl-2.2.53-9.fc33.x86_64.rpm",
+                "version": "2.2.53"
+            },
+            {
+                "arch": "noarch",
+                "check_gpg": true,
+                "checksum": "sha256:f4efaa5bc8382246d8230ece8bacebd3c29eb9fd52b509b1e6575e643953851b",
+                "epoch": 0,
+                "name": "basesystem",
+                "release": "10.fc33",
+                "remote_location": "https://mirrors.rit.edu/fedora/fedora/linux/releases/33/Everything/x86_64/os/Packages/b/basesystem-11-10.fc33.noarch.rpm",
+                "version": "11"
+            }
+	]}`
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	cmd, out, err := root.ExecuteTest("--json", "projects", "depsolve", "--distro=test-distro", "bash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, depsolveCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"name\": \"basesystem\"")
+	assert.Contains(t, string(stdout), "\"version\": \"2.2.53\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/projects/depsolve/bash?distro=test-distro\"")
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
@@ -136,6 +242,7 @@ func TestCmdProjectsDepsolveBadDistro(t *testing.T) {
 
 	// Get the compose types
 	cmd, out, err := root.ExecuteTest("projects", "depsolve", "--distro=homer", "bash")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.NotNil(t, err)
 	require.NotNil(t, out.Stdout)
@@ -148,6 +255,47 @@ func TestCmdProjectsDepsolveBadDistro(t *testing.T) {
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Contains(t, string(stderr), "DistroError: Invalid distro: homer")
+	assert.Equal(t, "GET", mc.Req.Method)
+}
+
+func TestCmdProjectsDepsolveBadDistroJSON(t *testing.T) {
+	// Test the "projects depsolve --distro=homer" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+        "errors": [
+            {
+                "id": "DistroError",
+                "msg": "Invalid distro: homer"
+            }
+        ],
+        "status": false
+}`
+
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Get the compose types
+	cmd, out, err := root.ExecuteTest("--json", "projects", "depsolve", "--distro=homer", "bash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, depsolveCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": false")
+	assert.Contains(t, string(stdout), "\"id\": \"DistroError\"")
+	assert.Contains(t, string(stdout), "\"msg\": \"Invalid distro: homer\"")
+	assert.Contains(t, string(stdout), "\"status\": 400")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
 	assert.Equal(t, "GET", mc.Req.Method)
 }
 
@@ -164,12 +312,14 @@ func TestCmdProjectsDepsolveUnknown(t *testing.T) {
 	}`
 	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
 		return &http.Response{
-			StatusCode: 200,
+			Request:    request,
+			StatusCode: 400,
 			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
 		}, nil
 	})
 
 	cmd, out, err := root.ExecuteTest("projects", "depsolve", "bash,homer")
+	require.NotNil(t, out)
 	defer out.Close()
 	require.NotNil(t, err)
 	assert.Equal(t, root.ExecutionError(cmd, ""), err)
@@ -183,5 +333,45 @@ func TestCmdProjectsDepsolveUnknown(t *testing.T) {
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Contains(t, string(stderr), "missing packages: homer")
+	assert.Equal(t, "GET", mc.Req.Method)
+}
+
+func TestCmdProjectsDepsolveUnknownJSON(t *testing.T) {
+	// Test the "projects depsolve" command with an unknown package
+	json := `{
+        "errors": [
+            {
+                "id": "ProjectsError",
+                "msg": "BadRequest: DNF error occured: MarkingErrors: Error occurred when marking packages for installation: Problems in request:\nmissing packages: homer"
+            }
+        ],
+        "status": false
+	}`
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	cmd, out, err := root.ExecuteTest("--json", "projects", "depsolve", "bash,homer")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	assert.Equal(t, root.ExecutionError(cmd, ""), err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, depsolveCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": false")
+	assert.Contains(t, string(stdout), "\"id\": \"ProjectsError\"")
+	assert.Contains(t, string(stdout), "\"msg\": \"BadRequest: DNF error occured")
+	assert.Contains(t, string(stdout), "\"status\": 400")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
 	assert.Equal(t, "GET", mc.Req.Method)
 }
--- cmd/composer-cli/projects/info_test.go
+++ cmd/composer-cli/projects/info_test.go
@@ -17,7 +17,7 @@ import (
 )
 
 func TestCmdProjectsInfo(t *testing.T) {
-	// Test the "modules list" command
+	// Test the "projects info" command
 	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
 		json := `{
     "projects": [
@@ -54,6 +54,7 @@ func TestCmdProjectsInfo(t *testing.T) {
 		}, nil
 	})
 
+	distro = ""
 	cmd, out, err := root.ExecuteTest("projects", "info", "bash")
 	defer out.Close()
 	require.Nil(t, err)
@@ -73,8 +74,8 @@ func TestCmdProjectsInfo(t *testing.T) {
 	assert.Equal(t, "/api/v1/projects/info/bash", mc.Req.URL.Path)
 }
 
-func TestCmdProjectsInfoDistro(t *testing.T) {
-	// Test the "modules info --distro=test-distro" command
+func TestCmdProjectsInfoJSON(t *testing.T) {
+	// Test the "projects info" command
 	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
 		json := `{
     "projects": [
@@ -111,6 +112,148 @@ func TestCmdProjectsInfoDistro(t *testing.T) {
 		}, nil
 	})
 
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "projects", "info", "bash")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"description\": \"The GNU Bourne Again shell")
+	assert.Contains(t, string(stdout), "\"version\": \"5.0.17\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/projects/info/bash\"")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/projects/info/bash", mc.Req.URL.Path)
+}
+
+func TestCmdModulesInfoUnknown(t *testing.T) {
+	// Test the "projects info" command with an unknown project
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+        "errors": [
+            {
+                "id": "UnknownProject",
+                "msg": "No packages have been found."
+            }
+        ],
+        "status": false
+}`
+
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("projects", "info", "mash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	assert.Equal(t, root.ExecutionError(cmd, ""), err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stdout)
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stderr), "UnknownProject: No packages have been found")
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/projects/info/mash", mc.Req.URL.Path)
+}
+
+func TestCmdModulesInfoUnknownJSON(t *testing.T) {
+	// Test the "projects info" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+        "errors": [
+            {
+                "id": "UnknownProject",
+                "msg": "No packages have been found."
+            }
+        ],
+        "status": false
+}`
+
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "projects", "info", "mash")
+	require.NotNil(t, out)
+	defer out.Close()
+	require.NotNil(t, err)
+	assert.Equal(t, root.ExecutionError(cmd, ""), err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": false")
+	assert.Contains(t, string(stdout), "\"id\": \"UnknownProject\"")
+	assert.Contains(t, string(stdout), "\"msg\": \"No packages have been found.\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/api/v1/projects/info/mash\"")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/projects/info/mash", mc.Req.URL.Path)
+}
+
+func TestCmdProjectsInfoDistro(t *testing.T) {
+	// Test the "projects info --distro=test-distro" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+    "projects": [
+        {
+            "builds": [
+                {
+                    "arch": "x86_64",
+                    "build_config_ref": "BUILD_CONFIG_REF",
+                    "build_env_ref": "BUILD_ENV_REF",
+                    "build_time": "2020-07-27T13:17:35",
+                    "changelog": "CHANGELOG_NEEDED",
+                    "epoch": 0,
+                    "metadata": {},
+                    "release": "2.fc33",
+                    "source": {
+                        "license": "GPLv3+",
+                        "metadata": {},
+                        "source_ref": "SOURCE_REF",
+                        "version": "5.0.17"
+                    }
+                }
+            ],
+            "description": "The GNU Bourne Again shell (Bash) is a shell or command language\ninterpreter that is compatible with the Bourne shell (sh). Bash\nincorporates useful features from the Korn shell (ksh) and the C shell\n(csh). Most sh scripts can be run by bash without modification.",
+            "homepage": "https://www.gnu.org/software/bash",
+            "name": "bash",
+            "summary": "The GNU Bourne Again shell",
+            "upstream_vcs": "UPSTREAM_VCS"
+        }
+    ]}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
 	cmd, out, err := root.ExecuteTest("projects", "info", "--distro=test-distro", "bash")
 	defer out.Close()
 	require.Nil(t, err)
@@ -123,6 +266,64 @@ func TestCmdProjectsInfoDistro(t *testing.T) {
 	assert.Contains(t, string(stdout), "Summary: The GNU Bourne Again shell")
 	assert.Contains(t, string(stdout), "             shell (sh). Bash")
 	assert.Contains(t, string(stdout), "     5.0.17-2.fc33.x86_64 at")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
+	assert.Equal(t, "GET", mc.Req.Method)
+	assert.Equal(t, "/api/v1/projects/info/bash", mc.Req.URL.Path)
+}
+
+func TestCmdProjectsInfoDistroJSON(t *testing.T) {
+	// Test the "projects info --distro=test-distro" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+    "projects": [
+        {
+            "builds": [
+                {
+                    "arch": "x86_64",
+                    "build_config_ref": "BUILD_CONFIG_REF",
+                    "build_env_ref": "BUILD_ENV_REF",
+                    "build_time": "2020-07-27T13:17:35",
+                    "changelog": "CHANGELOG_NEEDED",
+                    "epoch": 0,
+                    "metadata": {},
+                    "release": "2.fc33",
+                    "source": {
+                        "license": "GPLv3+",
+                        "metadata": {},
+                        "source_ref": "SOURCE_REF",
+                        "version": "5.0.17"
+                    }
+                }
+            ],
+            "description": "The GNU Bourne Again shell (Bash) is a shell or command language\ninterpreter that is compatible with the Bourne shell (sh). Bash\nincorporates useful features from the Korn shell (ksh) and the C shell\n(csh). Most sh scripts can be run by bash without modification.",
+            "homepage": "https://www.gnu.org/software/bash",
+            "name": "bash",
+            "summary": "The GNU Bourne Again shell",
+            "upstream_vcs": "UPSTREAM_VCS"
+        }
+    ]}`
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "projects", "info", "--distro=test-distro", "bash")
+	defer out.Close()
+	require.Nil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"description\": \"The GNU Bourne Again shell")
+	assert.Contains(t, string(stdout), "\"version\": \"5.0.17\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/projects/info/bash?distro=test-distro\"")
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Equal(t, []byte(""), stderr)
@@ -151,6 +352,7 @@ func TestCmdProjectsInfoBadDistro(t *testing.T) {
 	})
 
 	// Get the compose types
+	distro = ""
 	cmd, out, err := root.ExecuteTest("projects", "info", "--distro=homer", "bash")
 	defer out.Close()
 	require.NotNil(t, err)
@@ -164,5 +366,46 @@ func TestCmdProjectsInfoBadDistro(t *testing.T) {
 	stderr, err := ioutil.ReadAll(out.Stderr)
 	assert.Nil(t, err)
 	assert.Contains(t, string(stderr), "DistroError: Invalid distro: homer")
+	assert.Equal(t, "GET", mc.Req.Method)
+}
+
+func TestCmdProjectsInfoBadDistroJSON(t *testing.T) {
+	// Test the "projects info --distro=homer" command
+	mc := root.SetupCmdTest(func(request *http.Request) (*http.Response, error) {
+		json := `{
+        "errors": [
+            {
+                "id": "DistroError",
+                "msg": "Invalid distro: homer"
+            }
+        ],
+        "status": false
+}`
+
+		return &http.Response{
+			Request:    request,
+			StatusCode: 400,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	})
+
+	// Get the compose types
+	distro = ""
+	cmd, out, err := root.ExecuteTest("--json", "projects", "info", "--distro=homer", "bash")
+	defer out.Close()
+	require.NotNil(t, err)
+	require.NotNil(t, out.Stdout)
+	require.NotNil(t, out.Stderr)
+	require.NotNil(t, cmd)
+	assert.Equal(t, cmd, infoCmd)
+	stdout, err := ioutil.ReadAll(out.Stdout)
+	assert.Nil(t, err)
+	assert.Contains(t, string(stdout), "\"status\": false")
+	assert.Contains(t, string(stdout), "\"id\": \"DistroError\"")
+	assert.Contains(t, string(stdout), "\"msg\": \"Invalid distro: homer\"")
+	assert.Contains(t, string(stdout), "\"path\": \"/api/v1/projects/info/bash?distro=homer\"")
+	stderr, err := ioutil.ReadAll(out.Stderr)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(""), stderr)
 	assert.Equal(t, "GET", mc.Req.Method)
 }
