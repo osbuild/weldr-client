58a7433595371606232560962a128d3a1ab22055
Brian C. Lane <bcl@redhat.com>
2022-11-14 16:41:15 -0800

    composer-cli: Implement blueprints diff
    This adds a working 'blueprints diff' command where two commits for a
    blueprint can be compared using the commit hash returned by 'blueprints
    changes', or 'NEWEST' to select the most recent commit. Uncommitted
    workspace changes can be compared by passing 'WORKSPACE' instead of the
    commit id.
    
    It depends on the system diff utility and defaults to using colorized
    unified context diff output. The default diff arguments can be
    overridden by adding '--' to the end of the cmdline and passing diff
    arguments after that. These are passed directly to the diff utility.
    
    eg. output normal diff instead of unified context:
    
    ./composer-cli blueprints diff \
    fda3a8f9e589d1c423748b0408e5b71d9b769164 NEWEST -- --normal

--- cmd/composer-cli/blueprints/diff.go
+++ cmd/composer-cli/blueprints/diff.go
@@ -6,17 +6,26 @@ package blueprints
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
+	"github.com/osbuild/weldr-client/v2/cmd/composer-cli/root"
 	"github.com/spf13/cobra"
 )
 
 var (
 	diffCmd = &cobra.Command{
-		Use:   "diff BLUEPRINT FROM-COMMIT TO-COMMIT",
+		Use:   "diff BLUEPRINT FROM-COMMIT TO-COMMIT [-- DIFF-ARG ...]",
 		Short: "list the differences between two blueprint commits",
-		Long:  "list the differences between two blueprint commits where FROM-COMMIT is a commit hash or NEWEST, and TO-COMMIT is a commit hash, NEWEST, or WORKSPACE",
-		RunE:  diff,
-		Args:  cobra.ExactArgs(3),
+		Long: `diff lists the differences between two blueprint commits:
+    FROM-COMMIT is a commit hash or NEWEST,
+    and TO-COMMIT is a commit hash, NEWEST, or WORKSPACE
+  Arguments passed after -- are passed directly to the system diff utility.`,
+		Example: `  composer-cli blueprints diff simple HASH WORKSPACE
+  composer-cli blueprints diff simple HASH NEWEST -- -c --minimal`,
+		RunE: diff,
+		Args: cobra.MinimumNArgs(3),
 	}
 )
 
@@ -25,7 +34,195 @@ func init() {
 }
 
 func diff(cmd *cobra.Command, args []string) (rcErr error) {
-	fmt.Printf("Ran the blueprints diff: %v command\n", args)
+	// args[0] == blueprint name, args[1] = FROM-COMMIT, args[2] = TO-COMMIT
+	// args[3] and later are passed to the diff executable
+	if args[1] == "WORKSPACE" {
+		return root.ExecutionError(cmd, "FROM-COMMIT cannot be WORKSPACE")
+	}
 
+	if root.JSONOutput {
+		err := getBlueprintJSON(args[0], args[1])
+		if err != nil {
+			return root.ExecutionError(cmd, err.Error())
+		}
+
+		err = getBlueprintJSON(args[0], args[2])
+		if err != nil {
+			return root.ExecutionError(cmd, err.Error())
+		}
+		return nil
+	}
+
+	fromBlueprint, err := getBlueprint(args[0], args[1])
+	if err != nil {
+		return root.ExecutionError(cmd, err.Error())
+	}
+
+	toBlueprint, err := getBlueprint(args[0], args[2])
+	if err != nil {
+		return root.ExecutionError(cmd, err.Error())
+	}
+
+	// Everything after args[2] is passed to diff
+	// default to --color and -u if nothing passed
+	diffArgs := args[3:]
+	if len(diffArgs) == 0 {
+		diffArgs = []string{"--color", "-u"}
+	}
+
+	err = runDiff(fromBlueprint, args[1], toBlueprint, args[2], diffArgs)
+	if err != nil {
+		return root.ExecutionError(cmd, "%s", err)
+	}
+
+	return nil
+}
+
+func getBlueprint(name, commit string) (string, error) {
+	if commit == "WORKSPACE" {
+		// If nothing has been pushed to the /blueprints/workspace this will be the latest
+		bps, resp, err := root.Client.GetBlueprintsTOML([]string{name})
+		if err != nil {
+			return "", err
+		}
+		if resp != nil && !resp.Status {
+			return "", fmt.Errorf(strings.Join(resp.AllErrors(), ", "))
+		}
+		if len(bps) == 0 {
+			return "", fmt.Errorf("no blueprints")
+		}
+		return bps[0], nil
+	} else if commit == "NEWEST" {
+		// Get the list of changes for this blueprint
+		bps, resp, err := root.Client.GetBlueprintsChanges([]string{name})
+		if err != nil {
+			return "", err
+		}
+		if len(resp) > 0 {
+			return "", fmt.Errorf(resp[0].String())
+		}
+		if len(bps) < 1 {
+			return "", fmt.Errorf("no blueprints")
+		}
+		if len(bps[0].Changes) < 1 {
+			return "", fmt.Errorf("no NEWEST commit for %s", name)
+		}
+		commit = bps[0].Changes[0].Commit
+	}
+
+	// Return the blueprint commit
+	bp, resp, err := root.Client.GetBlueprintChangeTOML(name, commit)
+	if err != nil {
+		return "", err
+	}
+	if resp != nil && !resp.Status {
+		return "", fmt.Errorf(strings.Join(resp.AllErrors(), ", "))
+	}
+	return bp, nil
+}
+
+// runDiff runs the system diff utility from a temporary directory
+func runDiff(fromBlueprint, fromCommit, toBlueprint, toCommit string, diffArgs []string) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	// Make a temporary directory to save the blueprints into
+	// defer cleanup so it doesn't ever get left behind
+	tmpDir, err := os.MkdirTemp("", "bp-diff-*")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmpDir)
+	err = os.Chdir(tmpDir)
+	if err != nil {
+		return err
+	}
+	defer os.Chdir(cwd) //nolint:errcheck
+
+	// Use saveBlueprint from save.go
+	fromFilename, err := saveBlueprint(fromBlueprint, fromCommit, "")
+	if err != nil {
+		return err
+	}
+	diffArgs = append(diffArgs, fromFilename)
+
+	toFilename, err := saveBlueprint(toBlueprint, toCommit, "")
+	if err != nil {
+		return err
+	}
+	diffArgs = append(diffArgs, toFilename)
+
+	_, err = exec.LookPath("diff")
+	if err != nil {
+		return fmt.Errorf("The diff utility is required, please install it")
+	}
+
+	cmd := exec.Command("diff", diffArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		return fmt.Errorf("diff error: %s", err)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			// handle diff-specific return codes
+			// 0 == same file 1 == different file 2 == errors
+			if exitErr.ExitCode() > 1 {
+				return fmt.Errorf("diff error: %s", err)
+			}
+		} else {
+			// Not an exit error (IO, etc.) handle it normally
+			return err
+		}
+	}
+
+	return nil
+}
+
+// getBlueprintJSON is used for displaying the JSON
+func getBlueprintJSON(name, commit string) error {
+	if commit == "WORKSPACE" {
+		// If nothing has been pushed to the /blueprints/workspace this will be the latest
+		bps, resp, err := root.Client.GetBlueprintsJSON([]string{name})
+		if err != nil {
+			return err
+		}
+		if len(resp) > 0 {
+			return fmt.Errorf(resp[0].String())
+		}
+		if len(bps) == 0 {
+			return fmt.Errorf("no blueprints")
+		}
+		return nil
+	} else if commit == "NEWEST" {
+		// Get the list of changes for this blueprint
+		bps, resp, err := root.Client.GetBlueprintsChanges([]string{name})
+		if err != nil {
+			return err
+		}
+		if len(resp) > 0 {
+			return fmt.Errorf(resp[0].String())
+		}
+		if len(bps) < 1 {
+			return fmt.Errorf("no blueprints")
+		}
+		if len(bps[0].Changes) < 1 {
+			return fmt.Errorf("no NEWEST commit for %s", name)
+		}
+		commit = bps[0].Changes[0].Commit
+	}
+
+	// Get the blueprint commit
+	_, resp, err := root.Client.GetBlueprintChangeJSON(name, commit)
+	if err != nil {
+		return err
+	}
+	if resp != nil && !resp.Status {
+		return fmt.Errorf(strings.Join(resp.AllErrors(), ", "))
+	}
 	return nil
 }
