55e65ff2cab51b3ca6604e257d04abbf77208a96
Brian C. Lane <bcl@redhat.com>
2024-03-19 11:57:32 -0700

    cloud: Add StartCompose function
    This function uses the cloud API to start a compose using a blueprint
    that is included in the compose request instead of one that has been
    previously uploaded.
    
    Related: RHEL-60124

---
+++ cloud/compose_test.go
@@ -0,0 +1,44 @@
+package cloud
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/BurntSushi/toml"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStartCompose(t *testing.T) {
+	json := `{"href": "/api/image-builder-composer/v2/compose", "kind": "ComposeId", "id": "b9f75040-daf7-4470-b38e-e71ed74b5906"}`
+	mc := MockClient{
+		DoFunc: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 201,
+				Body:       io.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	tc := NewClient(context.Background(), &mc, "")
+
+	bp := `name="bp test"
+			version="1.1.1"`
+	var blueprint interface{}
+	err := toml.Unmarshal([]byte(bp), &blueprint)
+	require.Nil(t, err)
+
+	id, err := tc.StartCompose(blueprint, "minimal-raw", 0)
+	require.Nil(t, err)
+	assert.Equal(t, "b9f75040-daf7-4470-b38e-e71ed74b5906", id)
+	assert.Equal(t, "POST", mc.Req.Method)
+	assert.Equal(t, "/api/image-builder-composer/v2/compose", mc.Req.URL.Path)
+	body, err := io.ReadAll(mc.Req.Body)
+	mc.Req.Body.Close()
+	assert.Nil(t, err)
+	assert.Contains(t, string(body), "bp test")
+	assert.Contains(t, string(body), "1.1.1")
+	assert.Contains(t, string(body), "local_save")
+}
