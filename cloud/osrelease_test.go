805c7e47d30ee2d0bd8c27a50587f917437e0474
Brian C. Lane <bcl@redhat.com>
2024-03-19 11:57:32 -0700

    cloud: Add StartCompose function
    This function uses the cloud API to start a compose using a blueprint
    that is included in the compose request instead of one that has been
    previously uploaded.
    
    Related: RHEL-60124

---
+++ cloud/osrelease_test.go
@@ -0,0 +1,22 @@
+package cloud
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadOSRelease(t *testing.T) {
+	r := strings.NewReader(`
+NAME="Fedora Linux"
+VERSION="40 (Forty)"
+ID=fedora
+VERSION_ID=40
+`)
+
+	m, err := readOSRelease(r)
+	assert.Nil(t, err)
+	assert.Equal(t, "Fedora Linux", m["NAME"])
+	assert.Equal(t, "40", m["VERSION_ID"])
+}
