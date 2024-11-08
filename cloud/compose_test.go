e483a05a7e5bc4a2b50d2773e1c2c3154f36ec90
Brian C. Lane <bcl@redhat.com>
2024-11-08 14:08:51 -0800

    cloud: Add tests for ComposeWait function
    Related: RHEL-60148

--- cloud/compose_test.go
+++ cloud/compose_test.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/assert"
@@ -126,4 +127,106 @@ func TestComposeInfo(t *testing.T) {
 	assert.Equal(t, "ComposeStatus", info.Kind)
 	assert.Equal(t, "GET", mc.Req.Method)
 	assert.Equal(t, "/api/image-builder-composer/v2/composes/008fc5ad-adad-42ec-b412-7923733483a8", mc.Req.URL.Path)
+}
+
+func TestComposeWaitTimeout(t *testing.T) {
+	json := `{
+  "href": "/api/image-builder-composer/v2/composes/008fc5ad-adad-42ec-b412-7923733483a8",
+  "id": "008fc5ad-adad-42ec-b412-7923733483a8",
+  "kind": "ComposeStatus",
+  "status": "pending"
+}`
+
+	mc := MockClient{
+		DoFunc: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	tc := NewClient(context.Background(), &mc, "")
+
+	fiveSeconds, err := time.ParseDuration("5s")
+	assert.Nil(t, err)
+
+	// Interval must be less than timeout
+	aborted, _, err := tc.ComposeWait("008fc5ad-adad-42ec-b412-7923733483a8", fiveSeconds, time.Second)
+	assert.Nil(t, err)
+	assert.True(t, aborted)
+}
+
+func TestComposeWaitError(t *testing.T) {
+	json := `{"kind": "Error", "details": "testing error"}`
+	mc := MockClient{
+		DoFunc: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 400,
+				Body:       io.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	tc := NewClient(context.Background(), &mc, "")
+
+	tenSeconds, err := time.ParseDuration("10s")
+	assert.Nil(t, err)
+	sixtySeconds, err := time.ParseDuration("60s")
+	assert.Nil(t, err)
+
+	// Interval must be less than timeout
+	aborted, _, err := tc.ComposeWait("008fc5ad-adad-42ec-b412-7923733483a8", tenSeconds, sixtySeconds)
+	assert.NotNil(t, err)
+	assert.False(t, aborted)
+
+	// Test with server returning an error response
+	aborted, _, err = tc.ComposeWait("008fc5ad-adad-42ec-b412-7923733483a8", sixtySeconds, tenSeconds)
+	assert.NotNil(t, err)
+	assert.False(t, aborted)
+}
+
+func TestComposeWait(t *testing.T) {
+	json := `{
+  "href": "/api/image-builder-composer/v2/composes/008fc5ad-adad-42ec-b412-7923733483a8",
+  "id": "008fc5ad-adad-42ec-b412-7923733483a8",
+  "kind": "ComposeStatus",
+  "image_status": {
+    "status": "success",
+    "upload_status": {
+      "options": null,
+      "status": "success",
+      "type": "local"
+    },
+    "upload_statuses": [
+      {
+        "options": null,
+        "status": "success",
+        "type": "local"
+      }
+    ]
+  },
+  "status": "success"
+}`
+
+	mc := MockClient{
+		DoFunc: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       io.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	tc := NewClient(context.Background(), &mc, "")
+
+	tenSeconds, err := time.ParseDuration("10s")
+	assert.Nil(t, err)
+	sixtySeconds, err := time.ParseDuration("60s")
+	assert.Nil(t, err)
+
+	// Interval must be less than timeout
+	aborted, info, err := tc.ComposeWait("008fc5ad-adad-42ec-b412-7923733483a8", sixtySeconds, tenSeconds)
+	assert.Nil(t, err)
+	assert.False(t, aborted)
+	assert.Equal(t, "008fc5ad-adad-42ec-b412-7923733483a8", info.ID)
+	assert.Equal(t, "success", info.Status)
+	assert.Equal(t, "ComposeStatus", info.Kind)
 }
