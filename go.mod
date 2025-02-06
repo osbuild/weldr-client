a2370acc86a87c72d3bedfa67672ad2ae0eb36ed
Brian C. Lane <bcl@redhat.com>
2025-02-06 08:40:48 -0800

    go.mod: Bump go version to 1.22.6

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/osbuild/weldr-client/v2
 
-go 1.22.0
+go 1.22.6
 
 require (
 	github.com/BurntSushi/toml v1.4.0
--- tools/prepare-source.sh
+++ tools/prepare-source.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # Pin Go and toolbox versions at a reasonable version
-go get go@1.22.0 toolchain@1.22.0
+go get go@1.22.6 toolchain@1.22.6
 
 # Update go.mod and go.sum:
 go mod tidy
