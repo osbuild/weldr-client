6e072e8538134f59fcaa61637bec206c8dc69fd6
Brian C. Lane <bcl@redhat.com>
2025-02-10 14:15:31 -0800

    compose: Add cloudapi support to the compose types command
    If the cloudapi is available it will be used to list the compose types.
    This requires the distribution and arch names. They can be passed to the
    command with `--distro` and `--arch` but if either or both are missing
    the values from the host are used instead.
    
    Related: RHEL-60134

--- cmd/composer-cli/compose/types.go
+++ cmd/composer-cli/compose/types.go
@@ -11,6 +11,8 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/osbuild/weldr-client/v2/cmd/composer-cli/root"
+	"github.com/osbuild/weldr-client/v2/internal/common"
+	"github.com/osbuild/weldr-client/v2/weldr"
 )
 
 var (
@@ -19,25 +21,51 @@ var (
 		Short: "List the available compose types",
 		Example: `  composer-cli compose types
   composer-cli compose types --json
-  composer-cli compose types --distro fedora-36`,
+  composer-cli compose types --distro fedora-36
+  composer-cli compose types --distro fedora-36 --arch aarch64`,
 		RunE: types,
 		Args: cobra.NoArgs,
 	}
 	distro string
+	arch   string
 )
 
 func init() {
 	typesCmd.Flags().StringVarP(&distro, "distro", "", "", "Distribution")
+	typesCmd.Flags().StringVarP(&arch, "arch", "", "", "Architecture")
 	composeCmd.AddCommand(typesCmd)
 }
 
 func types(cmd *cobra.Command, args []string) error {
-	types, resp, err := root.Client.GetComposeTypes(distro)
-	if err != nil {
-		return root.ExecutionError(cmd, "Types Error: %s", err)
-	}
-	if resp != nil && !resp.Status {
-		return root.ExecutionErrors(cmd, resp.Errors)
+	var types []string
+	var err error
+	var resp *weldr.APIResponse
+
+	// First check the cloudapi, if available use that
+	if root.Cloud.Exists() {
+		if len(distro) == 0 {
+			distro, err = common.GetHostDistroName()
+			if err != nil {
+				return root.ExecutionError(cmd, "Types Error determining host distribution: %s", err)
+			}
+		}
+
+		if len(arch) == 0 {
+			arch = common.HostArch()
+		}
+
+		types, err = root.Cloud.GetComposeTypes(distro, arch)
+		if err != nil {
+			return root.ExecutionError(cmd, "Types Error: %s", err)
+		}
+	} else {
+		types, resp, err = root.Client.GetComposeTypes(distro)
+		if err != nil {
+			return root.ExecutionError(cmd, "Types Error: %s", err)
+		}
+		if resp != nil && !resp.Status {
+			return root.ExecutionErrors(cmd, resp.Errors)
+		}
 	}
 
 	sort.Strings(types)
