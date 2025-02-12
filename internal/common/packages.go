2517d7ef5db98a33cbdf8d0779ef504d587d638a
Brian C. Lane <bcl@redhat.com>
2025-02-12 13:45:05 -0800

    common: PackageNEVRA JSON epoch field can be string or int
    In the JSON returned from osbuild-composer the epoch can be a number
    when returned from the WELDR api, or it can be a string when returned
    from the Cloud API.
    
    This converts the json for either case so that the same structure can be
    used.
    
    Related: RHEL-60139

--- internal/common/packages.go
+++ internal/common/packages.go
@@ -1,6 +1,10 @@
 package common
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
 
 // PackageNEVRA contains the basic details about a package
 type PackageNEVRA struct {
@@ -18,4 +22,44 @@ func (pkg PackageNEVRA) String() string {
 	}
 
 	return fmt.Sprintf("%s-%d:%s-%s.%s", pkg.Name, pkg.Epoch, pkg.Version, pkg.Release, pkg.Arch)
+}
+
+// UnmarshalJSON is used to handle epoch possibly being an int or a string
+// The JSON we parse can have Epoch as a number or as a string so this
+// converts the string to an int in PackageNEVRA by using an intermediate
+// structure with Epoch as 'any' type
+func (pkg *PackageNEVRA) UnmarshalJSON(data []byte) error {
+	// Same as PackageNEVRA except that Epoch is any
+	var fuzzyNEVRA struct {
+		Arch    string `json:"arch"`
+		Epoch   any    `json:"epoch"`
+		Name    string `json:"name"`
+		Version string `json:"version"`
+		Release string `json:"release"`
+	}
+	if err := json.Unmarshal(data, &fuzzyNEVRA); err != nil {
+		return err
+	}
+
+	switch epoch := fuzzyNEVRA.Epoch.(type) {
+	case string:
+		var err error
+		pkg.Epoch, err = strconv.Atoi(epoch)
+		if err != nil {
+			return err
+		}
+	case float64:
+		pkg.Epoch = int(epoch)
+	case nil:
+		// Leave it as default of 0
+	default:
+		return fmt.Errorf("failed to convert epoch value \"%v\"/%T to number", fuzzyNEVRA.Epoch, fuzzyNEVRA.Epoch)
+	}
+
+	pkg.Arch = fuzzyNEVRA.Arch
+	pkg.Name = fuzzyNEVRA.Name
+	pkg.Version = fuzzyNEVRA.Version
+	pkg.Release = fuzzyNEVRA.Release
+
+	return nil
 }
--- internal/common/packages_test.go
+++ internal/common/packages_test.go
@@ -1,10 +1,12 @@
 package common
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPackageNEVRAString(t *testing.T) {
@@ -17,4 +19,34 @@ func TestPackageNEVRAString(t *testing.T) {
 	assert.Equal(t, "chrony-4.0-1.fc33.x86_64", fmt.Sprintf("%s", pkgList[0]))
 	//nolint:gosimple // using Sprintf on purpose
 	assert.Equal(t, "grub2-common-1:2.04-33.fc33.noarch", fmt.Sprintf("%s", pkgList[1]))
+}
+
+func TestPackageEpochString(t *testing.T) {
+	// Make sure json with epoch as a string works
+	j := `{"arch": "noarch", "epoch": "1", "name": "grub2-common", "version": "2.04", "release": "33.fc33"}`
+
+	var pkg PackageNEVRA
+	err := json.Unmarshal([]byte(j), &pkg)
+	require.NoError(t, err)
+	assert.Equal(t, PackageNEVRA{"noarch", 1, "grub2-common", "2.04", "33.fc33"}, pkg)
+}
+
+func TestPackageEpochInt(t *testing.T) {
+	// Make sure json with epoch as a number works
+	j := `{"arch": "noarch", "epoch": 1, "name": "grub2-common", "version": "2.04", "release": "33.fc33"}`
+
+	var pkg PackageNEVRA
+	err := json.Unmarshal([]byte(j), &pkg)
+	require.NoError(t, err)
+	assert.Equal(t, PackageNEVRA{"noarch", 1, "grub2-common", "2.04", "33.fc33"}, pkg)
+}
+
+func TestPackageEpochNil(t *testing.T) {
+	// Make sure json with missing epoch
+	j := `{"arch": "x86_64", "name": "chrony", "version": "4.0", "release": "1.fc33"}`
+
+	var pkg PackageNEVRA
+	err := json.Unmarshal([]byte(j), &pkg)
+	require.NoError(t, err)
+	assert.Equal(t, PackageNEVRA{"x86_64", 0, "chrony", "4.0", "1.fc33"}, pkg)
 }
