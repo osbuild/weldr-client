602c69a8d81e8e7f0457235e89111e2b21f99658
Brian C. Lane <bcl@redhat.com>
2025-02-11 16:22:19 -0800

    depsolve: Move parsing of weldr response into apischema
    This moves the details about the weldr api schema into the weldr api
    package and adds a test for the new ParseDepsolveResponse function.
    
    This will make it easier to add the cloud api depsolve support.
    
    Related: RHEL-60139

--- weldr/apischema_test.go
+++ weldr/apischema_test.go
@@ -5,6 +5,7 @@
 package weldr
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
@@ -92,4 +93,50 @@ func TestPackageString(t *testing.T) {
 	assert.Equal(t, "tmux-*", fmt.Sprintf("%s", Package{"tmux", "*"}))
 	//nolint:gosimple // using Sprintf on purpose
 	assert.Equal(t, "tmux-1.3", fmt.Sprintf("%s", Package{"tmux", "1.3"}))
+}
+
+func TestParseDepsolveResponse(t *testing.T) {
+	j := `[
+                {
+                    "blueprint": {
+                        "description": "Just tmux added",
+                        "distro": "",
+                        "groups": [],
+                        "modules": [],
+                        "name": "tmux-image",
+                        "packages": [
+                            {
+                                "name": "tmux"
+                            }
+                        ],
+                        "version": "0.0.1"
+                    },
+                    "dependencies": [
+                        {
+                            "arch": "x86_64",
+                            "check_gpg": true,
+                            "checksum": "sha256:c2bdd7e79bc2f882052e8d316beb2ac1608ea8e553f3735d007ff037f4129f83",
+                            "epoch": 0,
+                            "name": "authselect",
+                            "path": "Packages/a/authselect-1.5.0-8.fc41.x86_64.rpm",
+                            "release": "8.fc41",
+                            "remote_location": "http://opencolo.mm.fcix.net/fedora/linux/releases/41/Everything/x86_64/os/Packages/a/authselect-1.5.0-8.fc41.x86_64.rpm",
+                            "repo_id": "1e44fbb26eb25eb6cc9f5985a577d597e68cd0d3ae417cdb99030be4ead58ce4",
+                            "version": "1.5.0"
+                        }
+					]}]`
+
+	var data []interface{}
+	err := json.Unmarshal([]byte(j), &data)
+	require.NoError(t, err)
+
+	response, err := ParseDepsolveResponse(data)
+	require.NoError(t, err)
+	assert.Greater(t, len(response), 0)
+	assert.Equal(t, "tmux-image", response[0].Blueprint.Name)
+	assert.Equal(t, "0.0.1", response[0].Blueprint.Version)
+	require.Greater(t, len(response[0].Dependencies), 0)
+	assert.Equal(t, "authselect", response[0].Dependencies[0].Name)
+	assert.Equal(t, 0, response[0].Dependencies[0].Epoch)
+	assert.Equal(t, "1.5.0", response[0].Dependencies[0].Version)
 }
